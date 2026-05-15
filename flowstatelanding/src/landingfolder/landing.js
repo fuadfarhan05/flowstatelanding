9a9093e5984bb0c4162ce89c342a632f0bf1b424
Fuad Farhan <fuadhoquefarhan05@gmail.com>
2026-05-15 17:47:13 -0400

    removed sessionimg

--- flowstatelanding/src/landingfolder/landing.js
+++ flowstatelanding/src/landingfolder/landing.js
@@ -4,7 +4,6 @@ import "./landing.css";
 import UploadImg from "../images/interviewfromresume.webp";
 import FillerWordsImg from "../images/fillerwords.webp";
 import StarMethodImg from "../images/starmethod.webp";
-import SessionImg from "../images/sessions.png";
 import grade from "../images/grade.png";
 import logo from "../images/flowstatelogo.png";
 
