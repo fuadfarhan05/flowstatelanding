1b0766ad4c8e09c3a0aad4c966e03d9dea332b08
Fuad Farhan <fuadhoquefarhan05@gmail.com>
2026-06-12 21:40:00 -0400

    added new images

--- flowstatelanding/src/landingfolder/landing.js
+++ flowstatelanding/src/landingfolder/landing.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import "./landing.css";
 
-import UploadImg from "../images/interviewfromresume.webp";
+import UploadImg from "../images/resumetointerview.webp";
 import FillerWordsImg from "../images/fillerwordnew.webp";
-import StarMethodImg from "../images/starmethod.webp";
+import StarMethodImg from "../images/starmethodnew.webp";
 import grade from "../images/grade.png";
 import logo from "../images/flowstatelogo.png";
 
